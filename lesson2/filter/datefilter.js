b708ee690fd7678afdd733d7b89c8332e6720262
xjjwsh <19324662@qq.com>
2016-03-17 18:44:41 +0800

    Rejected commit: Default

--- lesson2/filter/datefilter.js
+++ lesson2/filter/datefilter.js
@@ -6,7 +6,8 @@ function access(req,res,next){
     if(now <13 || now >18){
         console.log("time is error");
         res.writeHead(503,{'Content-Type':'text/plain'});
-        res.end('abcdef');
+        res.end('abcd');
+
     }else{
         next();
     }
