0c0b35200f2710ae08c2ca7a118c55ff99b1170a
xjjwsh <19324662@qq.com>
2016-03-17 18:36:20 +0800

    Update datefilter.js

--- lesson2/filter/datefilter.js
+++ lesson2/filter/datefilter.js
@@ -6,7 +6,7 @@ function access(req,res,next){
     if(now <13 || now >18){
         console.log("time is error");
         res.writeHead(503,{'Content-Type':'text/plain'});
-        res.end('abc');
+        res.end('abcdef');
     }else{
         next();
     }
